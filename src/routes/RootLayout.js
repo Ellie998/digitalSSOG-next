6c680625b7602aea97d8b086ff7f924fa97067ca
Ellie <dpfla9144@naver.com>
2023-06-04 17:00:48 +0900

    JS - scrillToTop code refactoring

--- src/routes/RootLayout.js
+++ src/routes/RootLayout.js
@@ -1,18 +1,19 @@
-import { useRef, useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 import Header from "../Layouts/Header/Header";
 import Footer from "../Layouts/Footer/Footer";
-import ScrollToTop from "../components/ScrollToTop";
 
 import classes from "./RootLayout.module.css";
 
+const isWidthLess500 = window.screen.width < 500 ? true : false;
+
 function RootLayout({ children }) {
-  const divRef = useRef(null);
+  const divRef = useRef();
   const location = useLocation();
   const pathArray = location.pathname.split("/");
 
-  const scrollToTop = () => {
+  const pcScrollToTopCondition = () => {
     location.search === "" &&
       pathArray.length !== 5 &&
       divRef.current.scroll({
@@ -20,20 +21,42 @@ function RootLayout({ children }) {
         behavior: "smooth",
       });
   };
+  const pcScrollToTop = () => {
+    divRef.current.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  const mScrollToTopCondition = () => {
+    location.search === "" &&
+      pathArray.length !== 5 &&
+      window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
+  };
+  const mScrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   useEffect(() => {
-    scrollToTop();
+    isWidthLess500 ? mScrollToTopCondition() : pcScrollToTopCondition();
   }, [location]);
 
   return (
-    <div className={classes.layout} ref={divRef}>
-      <ScrollToTop></ScrollToTop>
+    <div className={classes.layout} id="pcScrollTarget" ref={divRef}>
       <Header />
 
       <Outlet />
       {children}
 
       <Footer />
+      <button onClick={isWidthLess500 ? mScrollToTop : pcScrollToTop}>
+        <i class="bi bi-arrow-up-short"></i>
+      </button>
     </div>
   );
 }
