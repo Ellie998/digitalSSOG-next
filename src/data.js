43a27d730206722a677cac993471a68210273a64
Ellie <dpfla9144@naver.com>
2023-06-13 15:10:08 +0900

    JS - data url change

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 const functionDataURL =
-  "https://gist.githubusercontent.com/Ellie998/c6fdd3f55e87ae24859255205c0bb8a6/raw/5144db522b732c76f3c14ea0c0cd4baf5bf96db9/functionData.json";
+  "https://gist.githubusercontent.com/Ellie998/c6fdd3f55e87ae24859255205c0bb8a6/raw/238915d0444dd49bbcea962ec8a648e9b9da41cc/functionData.json";
 
 const appDataURL =
   "https://gist.githubusercontent.com/Ellie998/7e12f7d3f5811c008341b317f86b342b/raw/e80b27658bfec8afa75bb90257c137e94162406a/appData.json";
