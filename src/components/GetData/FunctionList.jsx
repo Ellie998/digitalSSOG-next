7bb4d44da8a0309d6bee86feae221179ea7fcc7d
Ellie <dpfla9144@naver.com>
2023-06-16 11:02:31 +0900

    ACCESSIBILITY-CSS : add gap to functionList link

--- src/components/GetData/FunctionList.jsx
+++ src/components/GetData/FunctionList.jsx
@@ -1,15 +1,15 @@
-import styles from "./FunctionList.module.css";
+import classes from "./FunctionList.module.css";
 import MakeList from "./MakeList";
 import MakeListInCategory from "./MakeListInCategory";
 function FunctionList() {
   return (
-    <section className={styles.FunctionList}>
+    <section className={classes.FunctionList}>
       <h1>카테고리별 사용 가능한 기능</h1>
       <p>
         기능을 제공하는 어플 목록, 사용 방법을 알고 싶은 기능의 이름을
         클릭하세요.
       </p>
-      <div className={styles.box}>
+      <div className={classes.listBox}>
         <MakeList FunctionOrApp={"function"}></MakeList>
         <MakeListInCategory FunctionOrApp={"function"}></MakeListInCategory>
       </div>
