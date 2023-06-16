9a63597151ca9cc5057678824ec6c0852cbcdf63
Ellie <dpfla9144@naver.com>
2023-06-16 13:29:10 +0900

    JS-make functionNames to variable using useContext

--- src/components/AppDisplays/sections/AppMain.jsx
+++ src/components/AppDisplays/sections/AppMain.jsx
@@ -33,10 +33,28 @@ export const PageContext = createContext({
   methodId: "",
   descriptionId: "",
   urlContent: "",
+  functionName_makeCall: "",
+  functionName_getCall: "",
+  functionName_getVideoCall: "",
+  functionName_sendMessage: "",
+  functionName_resendMessage: "",
+  functionName_reserveMessage: "",
+  functionName_sendImg: "",
+  functionName_sendPhoneNum: "",
 });
 
 const appName_basic = "기본";
 const appName_kakaotalk = "카카오톡";
+const functionName_makeCall = "전화걸기(발신)";
+const functionName_getCall = "전화받기(수신)";
+const functionName_getVideoCall = "영상통화 발신";
+const functionName_sendMessage = "문자 발신";
+const functionName_seeMessage = "문자 수신";
+const functionName_resendMessage = "문자 전달";
+const functionName_reserveMessage = "예약 문자 발송";
+const functionName_sendImg = "이미지, 동영상 전송";
+const functionName_sendAudio = "오디오 전송";
+const functionName_sendPhoneNum = "연락처 공유";
 
 function AppMain() {
   const params = useParams();
@@ -52,7 +70,7 @@ function AppMain() {
   let choicedComponent = <div></div>;
 
   switch (realFunctionName) {
-    case "전화걸기(발신)":
+    case functionName_makeCall:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -79,7 +97,7 @@ function AppMain() {
         }
       }
       break;
-    case "전화받기(수신)":
+    case functionName_getCall:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <AnswerCall />;
@@ -98,7 +116,7 @@ function AppMain() {
         }
       }
       break;
-    case "영상통화 발신":
+    case functionName_getVideoCall:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -111,7 +129,7 @@ function AppMain() {
         }
       }
       break;
-    case "문자 발신":
+    case functionName_sendMessage:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -124,7 +142,7 @@ function AppMain() {
         }
       }
       break;
-    case "문자 수신":
+    case functionName_seeMessage:
       if (appName === appName_basic) {
         if (methodId === "1") {
           if (descriptionId === "0") {
@@ -147,7 +165,7 @@ function AppMain() {
         }
       }
       break;
-    case "문자 전달":
+    case functionName_resendMessage:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -168,7 +186,7 @@ function AppMain() {
         }
       }
       break;
-    case "예약 문자 발송":
+    case functionName_reserveMessage:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -185,7 +203,7 @@ function AppMain() {
         }
       }
       break;
-    case "이미지, 동영상 전송":
+    case functionName_sendImg:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -204,7 +222,7 @@ function AppMain() {
         }
       }
       break;
-    case "오디오 전송":
+    case functionName_sendAudio:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -221,7 +239,7 @@ function AppMain() {
         }
       }
       break;
-    case "연락처 공유":
+    case functionName_sendPhoneNum:
       if (appName === appName_basic) {
         if (descriptionId === "0") {
           choicedComponent = <MainApps />;
@@ -251,6 +269,15 @@ function AppMain() {
         methodId: methodId,
         descriptionId: descriptionId,
         urlContent: urlContent,
+        functionName_makeCall: functionName_makeCall,
+        functionName_getCall: functionName_getCall,
+        functionName_getVideoCall: functionName_getVideoCall,
+        functionName_sendMessage: functionName_sendMessage,
+        functionName_seeMessage: functionName_seeMessage,
+        functionName_resendMessage: functionName_resendMessage,
+        functionName_reserveMessage: functionName_reserveMessage,
+        functionName_sendImg: functionName_sendImg,
+        functionName_sendPhoneNum: functionName_sendPhoneNum,
       }}>
       {choicedComponent}
     </PageContext.Provider>
