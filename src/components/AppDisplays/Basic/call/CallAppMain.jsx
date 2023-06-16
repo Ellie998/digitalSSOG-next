16abe30c9fe02a638b54da12250d1f20adf9dea2
Ellie <dpfla9144@naver.com>
2023-06-16 19:22:17 +0900

    ADD CONTENT-call app tap and make animation

--- src/components/AppDisplays/Basic/call/CallAppMain.jsx
+++ src/components/AppDisplays/Basic/call/CallAppMain.jsx
@@ -7,12 +7,14 @@ import classes from "./CallAppMain.module.css";
 function CallAppMain() {
   const {
     appName,
+    methodId,
     urlContent,
     realFunctionName,
     functionName_makeCall,
     functionName_makeVideoCall,
     appName_basic,
   } = useContext(PageContext);
+  const [clickedTapName, setClickedTapName] = useState("키패드");
   const [clickedNum, changeNum] = useState("");
 
   function onchangeClickNum(event) {
@@ -38,6 +40,10 @@ function CallAppMain() {
 
   function onDeleteNum() {
     changeNum(clickedNum.slice(0, clickedNum.length - 1));
+  }
+
+  function changeTapHandler(event) {
+    setClickedTapName(event.target.innerText);
   }
 
   return (
@@ -80,30 +86,49 @@ function CallAppMain() {
               : null
           }>
           <div>
-            <i className="bi bi-camera-video"></i>
+            <i className="bi bi-camera-video-fill"></i>
           </div>
         </NavLink>
         <div>
           <NavLink
             data-tooltip={
               appName === appName_basic &&
-              realFunctionName === functionName_makeCall
+              realFunctionName === functionName_makeCall &&
+              methodId === "1"
                 ? `클릭!`
                 : null
             }
             to={
               appName === appName_basic &&
-              realFunctionName === functionName_makeCall
+              realFunctionName === functionName_makeCall &&
+              methodId === "1"
                 ? urlContent
                 : null
             }>
-            <div>
-              <i className="bi bi-telephone"></i>
+            <div className={classes.iconWrap_background}>
+              <i className="bi bi-telephone-fill"></i>
             </div>
           </NavLink>
         </div>
         <div>
           <i className="bi bi-arrow-left-short" onClick={onDeleteNum}></i>
+        </div>
+      </div>
+      <div className={classes.tabLayout}>
+        <div
+          onClick={changeTapHandler}
+          className={clickedTapName === "키패드" && classes.tap_clicked}>
+          키패드
+        </div>
+        <div
+          onClick={changeTapHandler}
+          className={clickedTapName === "최근기록" && classes.tap_clicked}>
+          최근기록
+        </div>
+        <div
+          onClick={changeTapHandler}
+          className={clickedTapName === "연락처" && classes.tap_clicked}>
+          연락처
         </div>
       </div>
     </div>
