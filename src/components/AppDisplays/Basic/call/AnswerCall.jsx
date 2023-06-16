90f2681f32c6627db3df3e5fe3aece5703d968a7
Ellie <dpfla9144@naver.com>
2023-06-16 12:59:55 +0900

    ADD CONTENT-get call kakaoapp

--- src/components/AppDisplays/Basic/call/AnswerCall.jsx
+++ src/components/AppDisplays/Basic/call/AnswerCall.jsx
@@ -4,26 +4,64 @@ import classes from "./ComeCall.module.css";
 import { NavLink } from "react-router-dom";
 
 const AnswerCall = () => {
-  const { urlContent } = useContext(PageContext);
+  const { urlContent, appName } = useContext(PageContext);
   return (
-    <section className={classes.section}>
-      <p className={classes.name}>이름</p>
-      <p className={classes.num}>010-0000-0000</p>
-      <div className={classes.btns}>
-        <NavLink to={`${urlContent}`}>
-          <div data-tooltip="클릭!">
-            <i className="bi bi-telephone" style={{ color: "blue" }}></i>
+    <>
+      {appName === "기본" && (
+        <div className={classes.layout}>
+          <div className={`${classes.content_title} ${classes.marginTop}`}>
+            이름
           </div>
-        </NavLink>
+          <div className={classes.content_sub}>010-0000-0000</div>
+          <div className={classes.btns}>
+            <NavLink to={`${urlContent}`}>
+              <div data-tooltip="클릭!">
+                <i
+                  className="bi bi-telephone-fill"
+                  style={{ color: "blue" }}></i>
+              </div>
+            </NavLink>
 
-        <div>
-          <i className="bi bi-telephone-x" style={{ color: "red" }}></i>
+            <div>
+              <i
+                className="bi bi-telephone-x-fill"
+                style={{ color: "red" }}></i>
+            </div>
+          </div>
+          <div className={classes.message}>
+            <p>문자 보내기</p>
+          </div>
         </div>
-      </div>
-      <div className={classes.message}>
-        <p>문자 보내기</p>
-      </div>
-    </section>
+      )}
+      {appName === "카카오톡" && (
+        <div className={classes.layout}>
+          <div className={classes["profileBox"]}>
+            <div>
+              <i className="bi bi-person-fill"></i>
+            </div>
+          </div>
+          <div className={classes.content_title}>홍길동</div>
+          <div
+            className={`${classes["content_sub"]} ${classes["font_color--light"]}`}>
+            보이스톡 해요.
+          </div>
+          <div className={classes.btns}>
+            <div>
+              <i
+                className="bi bi-telephone-x-fill"
+                style={{ color: "red" }}></i>
+            </div>
+            <NavLink to={`${urlContent}`}>
+              <div data-tooltip="클릭!">
+                <i
+                  className="bi bi-telephone-fill"
+                  style={{ color: "blue" }}></i>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/AppDisplays/Basic/call/CallConnected.jsx
+++ src/components/AppDisplays/Basic/call/CallConnected.jsx
@@ -1,39 +1,107 @@
+import { useContext } from "react";
 import classes from "./CallConnected.module.css";
+import { PageContext } from "../../sections/AppMain";
 
 const CallConnected = () => {
+  const { appName } = useContext(PageContext);
   return (
-    <section className={classes.layout}>
-      <div className={classes.headerNav}>...</div>
-      <div className={classes.phoneNum}>
-        <div>010-0000-0000</div>
-      </div>
-      <div className={classes.main_optionBox}>
-        <div>
-          <i className="bi bi-cassette"></i> <p>녹음</p>
-        </div>
-        <div>
-          <i className="bi bi-camera-video"></i> <p>영상통화</p>
-        </div>
-        <div>
-          <i className="bi bi-bluetooth"></i> <p>블루투스</p>
-        </div>
+    <>
+      {appName === "기본" && (
+        <section className={classes.layout}>
+          <div className={classes.headerNav}>...</div>
+          <div
+            className={`${classes.content_title} ${classes["marginTop--40"]}`}>
+            <div>010-0000-0000</div>
+          </div>
 
-        <div>
-          <i className="bi bi-volume-up"></i> <p>스피커</p>
-        </div>
-        <div>
-          <i className="bi bi-mic-mute"></i> <p>내 소리 차단</p>
-        </div>
-        <div>
-          <i className="bi bi-grip-horizontal"></i>
-          <p>키패드</p>
-        </div>
+          <div
+            className={`${classes.main_optionBox} ${classes["marginTop--30"]} `}>
+            <div>
+              <i className="bi bi-cassette-fill"></i> <div>녹음</div>
+            </div>
+            <div>
+              <i className="bi bi-camera-video-fill"></i> <div>영상통화</div>
+            </div>
+            <div>
+              <i className="bi bi-bluetooth"></i> <div>블루투스</div>
+            </div>
 
-        <div className={classes["main_optionIcon--redBack"]}>
-          <i className="bi bi-telephone-x"></i>
-        </div>
-      </div>
-    </section>
+            <div>
+              <i className="bi bi-volume-up-fill"></i> <div>스피커</div>
+            </div>
+            <div>
+              <i className="bi bi-mic-mute-fill"></i> <div>내 소리 차단</div>
+            </div>
+            <div>
+              <i className="bi bi-grip-horizontal"></i>
+              <div>키패드</div>
+            </div>
+
+            <div className={classes["main_optionIcon--redBack"]}>
+              <i className="bi bi-telephone-x-fill"></i>
+            </div>
+          </div>
+        </section>
+      )}
+      {appName === "카카오톡" && (
+        <section className={classes.layout}>
+          <div
+            className={`${classes["profileBox"]} ${classes["marginTop--10"]}`}>
+            <div>
+              <i className="bi bi-person-fill"></i>
+            </div>
+          </div>
+
+          <div className={classes.content_title}>
+            <div>홍길동</div>
+          </div>
+          <div className={classes["content_sub"]}>00:04</div>
+          <div
+            className={`${classes["main_optionBox--kakao"]} ${classes["marginTop--10"]}`}>
+            <div>
+              <div className={classes["main_optionIcon--border"]}>
+                <i className="bi bi-mic-mute-fill"></i>
+              </div>
+              <div>음소거</div>
+            </div>
+            <div>
+              <div className={classes["main_optionIcon--border"]}>
+                <i className="bi bi-volume-up-fill"></i>
+              </div>
+              <div>스피커</div>
+            </div>
+            <div>
+              <div className={classes["main_optionIcon--border"]}>
+                <i className="bi bi-filter"></i>
+              </div>
+              <div>음성필터</div>
+            </div>
+            <div>
+              <div className={classes["main_optionIcon--border"]}>
+                <i className="bi bi-plus-lg"></i>
+              </div>
+              <div>대화상대 초대</div>
+            </div>
+
+            <div>
+              <div className={classes["main_optionIcon--border"]}>
+                <i className="bi bi-arrows-angle-contract"></i>
+              </div>
+              <div>화면 숨김</div>
+            </div>
+            <div>
+              <div className={classes["main_optionIcon--border"]}>
+                <i className="bi bi-camera-video-fill"></i>
+              </div>
+              <div>페이스톡</div>
+            </div>
+          </div>
+          <div className={classes["main_optionIcon--redBack"]}>
+            <i className="bi bi-telephone-x-fill"></i>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
