7f1c58c9ed74ab589d8108202be399180752e868
Ellie <dpfla9144@naver.com>
2023-06-20 10:35:02 +0900

    CSS-add postit to index page page description list wrap

--- src/Layouts/Main/Index.js
+++ src/Layouts/Main/Index.js
@@ -16,17 +16,19 @@ function Index() {
         <p className={classes.subTitle}>
           사용중인 스마트 기기로 무엇을 할 수 있는지 알고 계시나요?
         </p>
-        <ul className={`${classes.textWrap}`}>
-          <li className={classes.boxShadow}>
+        <ul className={`${classes.listWrap}`}>
+          <li className={`${classes.postIt} ${classes.boxShadow}`}>
             <h3>
-              디지털쏙 페이지의 <br /> 핵심 포인트 1️⃣
+              디지털쏙 페이지의 <br />
+              핵심 포인트 <span> 1️⃣</span>
             </h3>
             <p> 스마트 기기로 할 수 있는 기능들을 정리했습니다.</p>
           </li>
-          <li className={classes.boxShadow}>
+          <li className={`${classes.postIt} ${classes.boxShadow}`}>
             <h3>
               디지털쏙 페이지의
-              <br /> 핵심 포인트 2️⃣
+              <br />
+              핵심 포인트 <span>2️⃣</span>
             </h3>
             <p>화면을 직접 눌러보며 기능을 미리 경험할 수 있습니다.</p>
           </li>
--- src/components/GetData/FunctionList.jsx
+++ src/components/GetData/FunctionList.jsx
@@ -4,8 +4,8 @@ import MakeListInCategory from "./MakeListInCategory";
 function FunctionList() {
   return (
     <section className={classes.layout}>
-      <h1>카테고리별 사용 가능한 기능</h1>
-      <p>
+      <h1 className={classes.title}>📌 카테고리별 사용 가능한 기능</h1>
+      <p className={classes.subTitle}>
         기능을 제공하는 어플 목록, 사용 방법을 알고 싶은 기능의 이름을
         클릭하세요.
       </p>
