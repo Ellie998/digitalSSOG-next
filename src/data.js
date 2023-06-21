e8e0acb7f5635aa1e6732cd6154396c14a8c6e1e
Ellie <dpfla9144@naver.com>
2023-06-21 16:25:49 +0900

    change description

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 const functionDataURL =
-  "https://gist.githubusercontent.com/Ellie998/c6fdd3f55e87ae24859255205c0bb8a6/raw/444ecba52365a2daffe24ea877e7d6a820d3127f/functionData.json";
+  "https://gist.githubusercontent.com/Ellie998/c6fdd3f55e87ae24859255205c0bb8a6/raw/54a7e398a334b67d4f54ec1708c0b3508ea358e0/functionData.json";
 
 const appDataURL =
   "https://gist.githubusercontent.com/Ellie998/7e12f7d3f5811c008341b317f86b342b/raw/e80b27658bfec8afa75bb90257c137e94162406a/appData.json";
