9fbfa4a040c0a30a3ad3d98511710b2c293e3f30
Ellie <dpfla9144@naver.com>
2023-06-28 14:15:55 +0900

    REMOVE google analytics code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import ReactGA from "react-ga";
+// import ReactGA from "react-ga";
 
 import reportWebVitals from "./reportWebVitals";
 import Index, { loader as functionDataLoader } from "./Layouts/Main/Index";
@@ -14,16 +14,16 @@ import ErrorPage from "./routes/ErrorPage";
 import "./common.css";
 
 // google search console
-const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
-ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
+// const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
+// ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
 
-const history = createBrowserHistory();
+// const history = createBrowserHistory();
 
-history.listen((response) => {
-  console.log(response.location.pathname);
-  ReactGA.set({ page: response.location.pathname });
-  ReactGA.pageview(response.location.pathname);
-});
+// history.listen((response) => {
+//   console.log(response.location.pathname);
+//   ReactGA.set({ page: response.location.pathname });
+//   ReactGA.pageview(response.location.pathname);
+// });
 
 const router = createBrowserRouter([
   {
