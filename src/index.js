a1b08dd62bec09c620f56b7d92a70cfc463e346f
Ellie <dpfla9144@naver.com>
2023-06-28 14:14:39 +0900

    REMOVE react-snap code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { hydrate } from "react-dom";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import ReactGA from "react-ga";
@@ -70,24 +69,12 @@ const router = createBrowserRouter([
   },
 ]);
 
-const app = (
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-if (root.hasChildNodes()) {
-  hydrate(app, root);
-} else {
-  root.render(app, root);
-  // render(<App />, root);
-}
-// root.render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
