3cc171d3fe415719855ed23260d2c2ee52f38138
Ellie <dpfla9144@naver.com>
2023-06-29 12:46:23 +0900

    remove react-snap for fix bulid error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
