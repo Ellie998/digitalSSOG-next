da035cad76b3130d5d79922c18cbf21c6f3231ec
Ellie <dpfla9144@naver.com>
2023-07-05 21:19:40 +0900

    logo.png move to public/images folder

--- components/DisplayBox/sections/AppMainError.jsx
+++ components/DisplayBox/sections/AppMainError.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import workHardImg from "../../../public/assets/work-hard.jpg";
+import workHardImg from "../../../public/images/work-hard.jpg";
 
 const AppMainError = () => {
   return (
