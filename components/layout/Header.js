98e48e9553469b5c98cdd13ba86d99e7f7628331
Ellie <dpfla9144@naver.com>
2023-07-05 20:58:47 +0900

    move logo.png to app floder

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import classes from "./Header.module.css";
-import logo from "../../public/assets/logo.png";
+import logo from "../../app/logo.png";
+// import logo from "../../public/assets/logo.png";
 
 function Header() {
   return (
