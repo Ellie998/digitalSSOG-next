eb62d077f21b861b88797c7bdcffb860c4e00e03
Ellie <dpfla9144@naver.com>
2023-07-06 17:11:36 +0900

    make sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,62 +6,62 @@ export default function sitemap() {
     },
     {
       // <!--전화받기-->
-      url: "https://ssog.pages.dev/description/%F0%9F%93%9E%EC%A0%84%ED%99%94%EB%B0%9B%EA%B8%B0(%EC%88%98%EC%8B%A0)",
+      url: "https://ssog.pages.dev/description/%EC%A0%84%ED%99%94%EB%B0%9B%EA%B8%B0(%EC%88%98%EC%8B%A0)",
       lastModified: new Date(),
     },
     {
       // <!--전화걸기-->
-      url: "https://ssog.pages.dev/description/%F0%9F%93%9E%EC%A0%84%ED%99%94%EA%B1%B8%EA%B8%B0(%EB%B0%9C%EC%8B%A0)",
+      url: "https://ssog.pages.dev/description/%EC%A0%84%ED%99%94%EA%B1%B8%EA%B8%B0(%EB%B0%9C%EC%8B%A0)",
       lastModified: new Date(),
     },
     {
       // <!--영상통화 발신-->
-      url: "https://ssog.pages.dev/description/%F0%9F%A4%B3%EC%98%81%EC%83%81%ED%86%B5%ED%99%94%20%EB%B0%9C%EC%8B%A0",
+      url: "https://ssog.pages.dev/description/%EC%98%81%EC%83%81%ED%86%B5%ED%99%94%20%EB%B0%9C%EC%8B%A0",
       lastModified: new Date(),
     },
     {
       // <!--문자 발신-->
-      url: "https://ssog.pages.dev/description/%F0%9F%92%AC%EB%AC%B8%EC%9E%90%20%EB%B0%9C%EC%8B%A0",
+      url: "https://ssog.pages.dev/description/%EB%AC%B8%EC%9E%90%20%EB%B0%9C%EC%8B%A0",
       lastModified: new Date(),
     },
     {
       // <!--문자 수신-->
-      url: "https://ssog.pages.dev/description/%F0%9F%92%AC%EB%AC%B8%EC%9E%90%20%EC%88%98%EC%8B%A0",
+      url: "https://ssog.pages.dev/description/%EB%AC%B8%EC%9E%90%20%EC%88%98%EC%8B%A0",
       lastModified: new Date(),
     },
     {
       // <!--예약문자 발송-->
-      url: "https://ssog.pages.dev/description/%E2%9C%89%EF%B8%8F%EC%98%88%EC%95%BD%20%EB%AC%B8%EC%9E%90%20%EB%B0%9C%EC%86%A1",
+      url: "https://ssog.pages.dev/description/%EC%98%88%EC%95%BD%20%EB%AC%B8%EC%9E%90%20%EB%B0%9C%EC%86%A1",
       lastModified: new Date(),
     },
     {
       // <!--문자 전달-->
-      url: "https://ssog.pages.dev/description/%E2%9C%89%EF%B8%8F%EB%AC%B8%EC%9E%90%20%EC%A0%84%EB%8B%AC",
+      url: "https://ssog.pages.dev/description/%EB%AC%B8%EC%9E%90%20%EC%A0%84%EB%8B%AC",
       lastModified: new Date(),
     },
     {
       // <!--이미지, 동영상 전송-->
-      url: "https://ssog.pages.dev/description/%F0%9F%93%81%EC%9D%B4%EB%AF%B8%EC%A7%80,%20%EB%8F%99%EC%98%81%EC%83%81%20%EC%A0%84%EC%86%A1",
+      url: "https://ssog.pages.dev/description/%EC%9D%B4%EB%AF%B8%EC%A7%80,%20%EB%8F%99%EC%98%81%EC%83%81%20%EC%A0%84%EC%86%A1",
       lastModified: new Date(),
     },
     {
       // <!--오디오 전송-->
-      url: "https://ssog.pages.dev/description/%F0%9F%93%81%EC%98%A4%EB%94%94%EC%98%A4%20%EC%A0%84%EC%86%A1",
+      url: "https://ssog.pages.dev/description/%EC%98%A4%EB%94%94%EC%98%A4%20%EC%A0%84%EC%86%A1",
       lastModified: new Date(),
     },
     {
       // <!--연락처 공유-->
-      url: "https://ssog.pages.dev/description/%E2%98%8E%EF%B8%8F%EC%97%B0%EB%9D%BD%EC%B2%98%20%EA%B3%B5%EC%9C%A0",
+      url: "https://ssog.pages.dev/description/%EC%97%B0%EB%9D%BD%EC%B2%98%20%EA%B3%B5%EC%9C%A0",
       lastModified: new Date(),
     },
     {
       // <!--그룹채팅방 나가기 및 초대 거부-->
-      url: "https://ssog.pages.dev/description/%F0%9F%99%8B%EA%B7%B8%EB%A3%B9%EC%B1%84%ED%8C%85%EB%B0%A9%20%EC%B4%88%EB%8C%80%EA%B1%B0%EB%B6%80%20%EB%B0%8F%20%EB%82%98%EA%B0%80%EA%B8%B0",
+      url: "https://ssog.pages.dev/description/%EA%B7%B8%EB%A3%B9%EC%B1%84%ED%8C%85%EB%B0%A9%20%EC%B4%88%EB%8C%80%EA%B1%B0%EB%B6%80%20%EB%B0%8F%20%EB%82%98%EA%B0%80%EA%B8%B0",
       lastModified: new Date(),
     },
     {
       // <!--그룹채팅방 대화 잠금-->
-      url: "https://ssog.pages.dev/description/%E2%9B%94%EF%B8%8F%EA%B7%B8%EB%A3%B9%EC%B1%84%ED%8C%85%EB%B0%A9%20%EC%B1%84%ED%8C%85%20%EC%9E%85%EB%A0%A5%EC%B0%BD%20%EC%9E%A0%EA%B8%88%ED%95%98%EA%B8%B0",
+      url: "https://ssog.pages.dev/description/%EA%B7%B8%EB%A3%B9%EC%B1%84%ED%8C%85%EB%B0%A9%20%EC%B1%84%ED%8C%85%20%EC%9E%85%EB%A0%A5%EC%B0%BD%20%EC%9E%A0%EA%B8%88%ED%95%98%EA%B8%B0",
       lastModified: new Date(),
     },
   ];
