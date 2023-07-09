980ade5570d5195076d285d4a80137eb4ba6b9fe
Ellie <dpfla9144@naver.com>
2023-07-09 09:28:42 +0900

    remove useless code

--- app/description/[...functionName]/page.jsx
+++ app/description/[...functionName]/page.jsx
@@ -1,6 +1,5 @@
 // app/description/[functionName]/page.jsx
 
-import Head from "next/head";
 import FunctionDescriptionProvider from "../../../components/FunctionDescription/FunctionDescriptionProvider";
 import MyLayout from "../../../components/layout/MyLayout";
 
@@ -70,74 +69,6 @@ export async function generateMetadata({ params }) {
 export default function Page() {
   return (
     <MyLayout>
-      {/* <Head>
-        <meta
-          name="naver-site-verification"
-          content="55145f147d68935311d0493b0428d0a9843e5eb9"
-        />
-
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{`${functionName} 기능 사용법 - 디지털쏙`}</title>
-        <meta
-          name="description"
-          content={`${functionName} 기능을 사용하는 여러가지 방법을 공유합니다.`}
-        />
-        <meta name="keywords" content="digital,app,function,description,tech" />
-        <meta itemprop="image" content="/assets/metaIMG.png" />
-        <meta
-          property="url"
-          content={`https://ssog.pages.dev/description/${functionName}`}
-        />
-        <meta property="type" content="website" />
-        <meta property="og:title" content="디지털쏙" />
-        <meta
-          property="og:description"
-          content={`${functionName} 기능을 사용하는 여러가지 방법을 공유합니다.`}
-        />
-        <meta itemprop="og:image" content="/assets/metaIMG.png" />
-
-        <meta
-          property="og:url"
-          content={`https://ssog.pages.dev/description/${functionName}`}
-        />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="디지털쏙" />
-        <meta
-          name="twitter:description"
-          content={`${functionName} 기능을 사용하는 여러가지 방법을 공유합니다.`}
-        />
-        <meta itemprop="twitter:image" content="/assets/metaIMG.png" />
-
-        <meta
-          property="twitter:url"
-          content={`https://ssog.pages.dev/description/${functionName}`}
-        />
-        <meta property="twitter:type" content="website" />
-
-        <link rel="icon" href="/assets/favicon/favicon.ico" />
-        <link
-          rel="icon"
-          href="/assets/favicon/favicon-32x32.png"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          rel="icon"
-          href="/assets/favicon/favicon-16x16.png"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/assets/favicon/apple-touch-icon.png"
-          sizes="180x180"
-          type="image/png"
-        />
-        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
-      </Head> */}
       <FunctionDescriptionProvider></FunctionDescriptionProvider>
     </MyLayout>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 // app/page.jsx
-import Head from "next/head";
 import Index from "../components/layout/Index";
 import MyLayout from "../components/layout/MyLayout";
 
@@ -72,66 +71,6 @@ export const metadata = {
 export default function Page() {
   return (
     <MyLayout>
-      {/* <Head>
-        <meta
-          name="naver-site-verification"
-          content="55145f147d68935311d0493b0428d0a9843e5eb9"
-        />
-
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>디지털쏙</title>
-        <meta
-          name="description"
-          content="디지털쏙 페이지에서 스마트 기기로 할 수 있는 다양한 기능들을 만나 보세요."
-        />
-        <meta name="keywords" content="digital,app,function,description,tech" />
-        <meta itemprop="image" content="/assets/metaIMG.png" />
-        <meta property="url" content={`https://ssog.pages.dev`} />
-        <meta property="type" content="website" />
-        <meta property="og:title" content="디지털쏙" />
-        <meta
-          property="og:description"
-          content="디지털쏙 페이지에서 스마트 기기로 할 수 있는 다양한 기능들을 만나 보세요."
-        />
-        <meta itemprop="og:image" content="/assets/metaIMG.png" />
-
-        <meta property="og:url" content={`https://ssog.pages.dev`} />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="디지털쏙" />
-        <meta
-          name="twitter:description"
-          content="디지털쏙 페이지에서 스마트 기기로 할 수 있는 다양한 기능들을 만나 보세요."
-        />
-        <meta itemprop="twitter:image" content="/assets/metaIMG.png" />
-
-        <meta property="twitter:url" content={`https://ssog.pages.dev`} />
-        <meta property="twitter:type" content="website" />
-
-        <link rel="icon" href="/assets/favicon/favicon.ico" />
-        <link
-          rel="icon"
-          href="/assets/favicon/favicon-32x32.png"
-          sizes="32x32"
-          type="image/png"
-        />
-        <link
-          rel="icon"
-          href="/assets/favicon/favicon-16x16.png"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/assets/favicon/apple-touch-icon.png"
-          sizes="180x180"
-          type="image/png"
-        />
-        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
-      </Head> */}
-
       <Index></Index>
     </MyLayout>
   );
