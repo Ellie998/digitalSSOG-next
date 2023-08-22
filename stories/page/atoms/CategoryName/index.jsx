2176d17c72d118d93dcc37e08295dd2244c099d5
Ellie <dpfla9144@naver.com>
2023-08-22 15:44:55 +0900

    fix font didn't applyed well-CategoryName

--- stories/page/atoms/CategoryName/index.jsx
+++ stories/page/atoms/CategoryName/index.jsx
@@ -3,32 +3,15 @@ import styled from "@emotion/styled";
 import Link from "next/link";
 
 const Container = styled.li`
-  @font-face {
-    font-family: "Cafe24Shiningstar";
-    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Shiningstar.woff")
-      format("woff");
-    font-weight: normal;
-    font-style: normal;
-    font-display: swap;
-  }
   list-style: none;
-
   padding: 0 0 10px 0;
   display: inline-block;
   margin-left: 0.5rem;
   min-width: max-content;
-  transition: 0.1s;
-
   max-height: 40px;
-
-  font-family: "Cafe24Shiningstar";
-  font-size: 1.5rem;
-  font-weight: ${(props) => (props.isClicked ? "bold" : "normal")};
+  /*  */
   position: ${(props) => (props.isClicked ? "relative" : "block")};
-
-  &:hover {
-    font-weight: 900;
-  }
+  transition: 0.1s;
 
   &:after {
     content: "";
@@ -50,8 +33,25 @@ const Container = styled.li`
       width: 0%;
     }
     100% {
-      width: 95%;
+      width: 105%;
     }
+  }
+`;
+const ParagraphContainer = styled.p`
+  @font-face {
+    font-family: "Cafe24Shiningstar";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Shiningstar.woff")
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+  }
+  font-family: "Cafe24Shiningstar";
+  font-size: 1.5rem;
+  font-weight: ${(props) => (props.isClicked ? "bold" : "normal")};
+
+  &:hover {
+    font-weight: 900;
   }
 `;
 // eslint-disable-next-line react/prop-types
@@ -65,13 +65,10 @@ const CategoryName = ({ children, name, tabName, setTabName }) => {
   }
   return (
     <Container isClicked={tabName === name}>
-      <Link
-        scroll={false}
-        onClick={linkClickHandler}
-        className={tabName === name ? "font-bold" : ""}
-        href={{ query: { tab: name } }}
-        id={name}>
-        {children}
+      <Link scroll={false} href={{ query: { tab: name } }}>
+        <ParagraphContainer onClick={linkClickHandler} id={name}>
+          {children}
+        </ParagraphContainer>
       </Link>
     </Container>
   );
