e717568b42c8d4acc0f15e73a83ef97ec22d2ade
Ellie <dpfla9144@naver.com>
2023-09-05 15:15:15 +0900

    add google analytics code

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,16 +5,18 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="ko" suppressHydrationWarning={true}>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-NRN6NT762E" />
-      <Script id="google-analytics">
-        {`
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-NRN6NT762E" />
+        <Script id="google-analytics">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           
           gtag('config', 'G-NRN6NT762E');
           `}
-      </Script>
+        </Script>
+      </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
