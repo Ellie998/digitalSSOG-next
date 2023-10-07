245207ad4e45d99be9cb61bdbb496fb886c62b2e
Ellie <dpfla9144@naver.com>
2023-10-07 16:20:15 +0900

    fix path mistake

--- stories/page/organisms/IndexFunctionsPostit/index.jsx
+++ stories/page/organisms/IndexFunctionsPostit/index.jsx
@@ -6,9 +6,9 @@ const Container = styled.div`
   max-width: 90%;
 `;
 
-import CategoryContentList from "stories/page/organisms/CategoryContentList";
-import CategoryList from "stories/page/organisms/CategoryList";
-import PostitMedium from "stories/page/molecules/PostitMedium";
+import CategoryContentList from "stories/page/organisms/CategoryContentList/index";
+import CategoryList from "stories/page/organisms/CategoryList/index";
+import PostitMedium from "stories/page/molecules/PostitMedium/index";
 
 // eslint-disable-next-line react/prop-types
 const IndexFunctionsPostit = ({ setTabName, tabName }) => {
