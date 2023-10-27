521043ed105b16ea530eaa8f67020600a2624e0a
Ellie <dpfla9144@naver.com>
2023-10-27 16:11:34 +0900

    come back code - IndexFunctionNote

--- stories/page/organisms/IndexFunctionNote/index.jsx
+++ stories/page/organisms/IndexFunctionNote/index.jsx
@@ -1,20 +1,24 @@
+import styled from "@emotion/styled";
 import IndexFunctionsPostit from "stories/page/organisms/IndexFunctionsPostit/index";
 import Note from "stories/page/molecules/Note/index";
 
+const Container = styled.section`
+  margin: 0 auto;
+  max-width: 800px;
+`;
+
 // eslint-disable-next-line react/prop-types, no-unused-vars
-const IndexFunntionNote = () => {
+const IndexFunntionNote = ({ tabName, setTabName }) => {
   return (
-    <section className="max-w-3xl mx-auto my-0">
+    <Container>
       <Note
         title={"스마트 기기로 사용 가능한 기능들"}
         subTitle={
           "카테고리를 누르면 카테고리에 속하는 기능들을 볼 수 있습니다."
         }
         holeNum={17}>
-        <IndexFunctionsPostit />
+        <IndexFunctionsPostit tabName={tabName} setTabName={setTabName} />
       </Note>
-    </section>
+    </Container>
   );
 };
-
-export default IndexFunntionNote;
