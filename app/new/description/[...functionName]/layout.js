c2b5998ba586695699de2c951aa55e95f83f9918
Ellie <dpfla9144@naver.com>
2023-11-15 15:41:34 +0900

    remove new folder

--- app/new/description/[...functionName]/layout.js
+++
@@ -1,3 +0,0 @@
-export default function NewDescriptionLayout({ children }) {
-  return <>{children}</>;
-}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
-// app/page.jsx
-import Index from "../components/layout/Index";
-import MyLayout from "../components/layout/MyLayout";
-
+import Note from "component/server/molecules/Note/index";
+import IndexFunctionsPostit from "component/server/organisms/IndexFunctionsPostit/index";
+import IndexIntro from "component/server/templates/IndexIntro/index";
+import MyLayout from "components/layout/MyLayout";
 export const runtime = "edge";
 
 export const metadata = {
@@ -77,12 +77,23 @@ export const metadata = {
   },
 };
 
-export default function Page() {
+export default function Page(props) {
   return (
-    <>
-      <MyLayout>
-        <Index></Index>
-      </MyLayout>
-    </>
+    <MyLayout>
+      <IndexIntro />
+
+      <main>
+        <section className="max-w-3xl mx-auto my-0">
+          <Note
+            title={"스마트 기기로 사용 가능한 기능들"}
+            subTitle={
+              "카테고리를 누르면 카테고리에 속하는 기능들을 볼 수 있습니다."
+            }
+            holeNum={17}>
+            <IndexFunctionsPostit tabName={props.searchParams.tab} />
+          </Note>
+        </section>
+      </main>
+    </MyLayout>
   );
 }
