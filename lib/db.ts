affae0aca99301ae0634a3614863591fb00a5b81
Ellie <dpfla9144@naver.com>
2023-12-07 15:08:34 +0900

    add prisma model

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
-async function main() {
-  // ... you will write your Prisma Client queries here
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// globalThis는 전역객체에 접근할 수 있는 식별자
+export const db = globalThis.prisma || new PrismaClient();
+// use `prisma` in your application to read and write data in your DB
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+// export const db = new PrismaClient();
