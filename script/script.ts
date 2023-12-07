b1d144b51eff653725e1b21ab692741d8eb3d5d2
Ellie <dpfla9144@naver.com>
2023-12-07 15:17:16 +0900

    add categories into db

--- script/script.ts
+++ script/script.ts
@@ -1,8 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import { db } from "../lib/db";
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  const categories = await db.function_category.createMany({
+    data: [
+      {
+        name: "sns",
+        icon: "❤️",
+      },
+      {
+        name: "문자",
+        icon: "💬",
+      },
+      {
+        name: "전화",
+        icon: "📞",
+      },
+    ],
+  });
+  console.log("Category Add Success");
+}
 
 main()
   .then(async () => {
