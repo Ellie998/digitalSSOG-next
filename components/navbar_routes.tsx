cdb443462b90e1644e71789c56a368eb90a87323
Ellie <dpfla9144@naver.com>
2023-12-15 16:14:12 +0900

    fix build error - window.location error

--- components/navbar_routes.tsx
+++ components/navbar_routes.tsx
@@ -3,11 +3,14 @@
 import { BsPersonCircle } from "react-icons/bs";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { getLocationOrigin } from "next/dist/shared/lib/utils";
 
 const NavbarRoutes = () => {
+  const location = getLocationOrigin();
+
   return (
     <div className="flex ml-auto gap-x-2">
-      {window.location.hostname === "localhost" && (
+      {location.includes("localhost") && (
         <Button variant={"secondary"}>
           <Link href={`/admin`}>Admin</Link>
         </Button>
