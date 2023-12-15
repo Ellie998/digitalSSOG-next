d835a9356d943eb18fc4cf00d3cf0adc1cc81544
Ellie <dpfla9144@naver.com>
2023-12-15 16:25:43 +0900

    fix error - window.location in NavbarRoutes

--- components/navbar_routes.tsx
+++ components/navbar_routes.tsx
@@ -1,16 +1,11 @@
-"use client";
-
 import { BsPersonCircle } from "react-icons/bs";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { getLocationOrigin } from "next/dist/shared/lib/utils";
 
 const NavbarRoutes = () => {
-  const location = getLocationOrigin();
-
   return (
     <div className="flex ml-auto gap-x-2">
-      {location.includes("localhost") && (
+      {typeof window === "undefined" && (
         <Button variant={"secondary"}>
           <Link href={`/admin`}>Admin</Link>
         </Button>
