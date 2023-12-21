6c1e361cf75c3b243f27fb4d74bf8136e1d1a046
Ellie <dpfla9144@naver.com>
2023-12-21 15:56:46 +0900

    add style : error page

--- app/(blog)/(error)/error/page.jsx
+++ app/(blog)/(error)/error/page.jsx
@@ -2,7 +2,7 @@
 
 const ErrorPage = ({ statusCode }) => {
   return (
-    <main style={{ textAlign: "center" }}>
+    <main style={{ textAlign: "center", paddingTop: "200px" }}>
       <h1>Error Occured!</h1>
       <p>{statusCode}</p>
     </main>
