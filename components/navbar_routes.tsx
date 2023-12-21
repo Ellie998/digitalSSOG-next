9cbd0f21351814a857d6c9729628ed2117a22b64
Ellie <dpfla9144@naver.com>
2023-12-21 19:36:23 +0900

    fix error - admin button

--- components/navbar_routes.tsx
+++ components/navbar_routes.tsx
@@ -1,11 +1,14 @@
+import { headers } from "next/headers";
 import { BsPersonCircle } from "react-icons/bs";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
 const NavbarRoutes = () => {
+  const headersList = headers();
+
   return (
     <div className="flex ml-auto gap-x-2">
-      {typeof window === "undefined" && (
+      {headersList.get("host")?.includes("localhost") && (
         <Button variant={"secondary"}>
           <Link href={`/admin`}>Admin</Link>
         </Button>
