ea380ddd13733181f7e9c1c97628520c2e2accbd
Ellie <dpfla9144@naver.com>
2024-01-03 15:51:15 +0900

    add style - main category tab

--- app/(blog)/(routes)/(root)/_components/categories.tsx
+++ app/(blog)/(routes)/(root)/_components/categories.tsx
@@ -1,27 +1,34 @@
 import { db } from "@/lib/db";
 import Link from "next/link";
+import classes from "./categories.module.css";
 
-const Categorires = async () => {
+const Categorires = async ({ tab }: { tab: string }) => {
   const categories = await db.function_category.findMany({
     where: {},
   });
 
   return (
-    <div>
-      <div>
-        {categories?.map((category) => (
+    <div className="flex flex-wrap px-4 mx-auto text-center gap-x-5 w-fit sm:px-0">
+      <div className={`${classes.index}`}>카테고리 : </div>
+      {categories?.map((category, i) => (
+        <div className={`flex ${classes.categoryList}`} key={i}>
           <Link
             href={{ query: { tab: category.name.replaceAll(" ", "-") } }}
             scroll={false}
             key={category.id}>
-            <div className="flex text-lg gap-x-2">
+            <div
+              className={` flex text-lg ${
+                tab === category.name.replaceAll(" ", "-")
+                  ? classes.clicked
+                  : ""
+              } `}>
               <div>{category.icon}</div>
-              <div>{category.name}</div>
+              <div className={`${classes.category}`}>{category.name}</div>
             </div>
           </Link>
-        ))}
-      </div>
-      <div></div>
+          {categories.length - 1 !== i ? "," : ""}
+        </div>
+      ))}
     </div>
   );
 };
--- app/(blog)/(routes)/(root)/page.tsx
+++ app/(blog)/(routes)/(root)/page.tsx
@@ -11,8 +11,8 @@ const BlogPage = ({ searchParams }: { searchParams: { tab: string } }) => {
       title={"스마트 기기로 사용 가능한 기능들"}
       subTitle={"카테고리를 누르면 카테고리에 속하는 기능들을 볼 수 있습니다."}>
       <PostitMedium
-        className="w-4/5 col-start-2 col-end-3 mx-auto"
-        title={<Categorires />}
+        className="w-full col-start-2 col-end-3 mx-auto mb-10 md:w-4/5"
+        title={<Categorires tab={searchParams.tab} />}
         content={<Functions searchParams={searchParams} />}
       />
     </Note>
