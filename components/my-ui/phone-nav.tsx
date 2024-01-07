782fc860fb331f5e56bd3ba9b977bdfc8a3afc91
Ellie <dpfla9144@naver.com>
2024-01-07 16:38:12 +0900

    fix error

--- components/my-ui/phone-nav.tsx
+++ components/my-ui/phone-nav.tsx
@@ -3,20 +3,24 @@ import Flex from "../phone-ui/flex";
 
 function PhoneNav() {
   return (
-    <Flex
+    <div
       style={{
         width: "170px",
-        zIndex: "1",
+        zIndex: "10",
         fontSize: "0.7rem",
         gridRow: "5/6",
         padding: "0 10px",
-      }}
-      items={[
-        <BsList key="list" />,
-        <BsSquare key="home" />,
-        <BsChevronDoubleLeft key="back" />,
-      ]}
-    />
+      }}>
+      <Flex
+        style={{ width: "170px" }}
+        items={[
+          <BsList key="list" />,
+          <BsSquare key="home" />,
+          <BsChevronDoubleLeft key="back" />,
+        ]}
+        children={undefined}
+      />
+    </div>
   );
 }
 
