0922d528a4eb995936df4c0a88ae04a4fc30633e
Ellie <dpfla9144@naver.com>
2024-01-12 20:28:45 +0900

    change note margin

--- app/(blog)/(routes)/(root)/page.tsx
+++ app/(blog)/(routes)/(root)/page.tsx
@@ -8,19 +8,21 @@ import Loading from "./loading.js";
 const BlogPage = ({ searchParams }: { searchParams: { tab: string } }) => {
   return (
     <Suspense fallback={<Loading />}>
-      <Note
-        className="w-4/5 mx-auto mt-20 "
-        holeNum={17}
-        title={"스마트 기기로 사용 가능한 기능들"}
-        subTitle={
-          "카테고리를 누르면 카테고리에 속하는 기능들을 볼 수 있습니다."
-        }>
-        <PostitMedium
-          className="w-full col-start-2 col-end-3 mx-auto mb-10 md:w-4/5"
-          title={<Categorires tab={searchParams.tab} />}
-          content={<Functions searchParams={searchParams} />}
-        />
-      </Note>
+      <div className="py-20 ">
+        <Note
+          className="w-4/5 mx-auto "
+          holeNum={17}
+          title={"스마트 기기로 사용 가능한 기능들"}
+          subTitle={
+            "카테고리를 누르면 카테고리에 속하는 기능들을 볼 수 있습니다."
+          }>
+          <PostitMedium
+            className="w-full col-start-2 col-end-3 mx-auto mb-10 md:w-4/5"
+            title={<Categorires tab={searchParams.tab} />}
+            content={<Functions searchParams={searchParams} />}
+          />
+        </Note>
+      </div>
     </Suspense>
   );
 };
