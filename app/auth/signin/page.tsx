fc8442bf96b24131f8830192649b681512646f98
Ellie <dpfla9144@naver.com>
2024-02-07 17:27:09 +0900

    add temp valification in login form

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,19 +1,40 @@
 'use client';
 import * as React from 'react';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 
 import { Button } from '@/components/ui/button';
-import { CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+
 import { Separator } from '@/components/ui/separator';
-import Link from 'next/link';
-
 import Postit from '../_components/postit';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
+const schema = z.object({
+  email: z.string().min(1, { message: 'Required' }),
+  pw: z.string().min(8),
+});
 const AuthPage = () => {
-  const [enteredEmail, setEnteredEmail] = React.useState('');
-  const [enteredPassword, setEnteredPassword] = React.useState('');
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
 
+  function onSubmit(values: z.infer<typeof schema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <div className="flex items-center justify-center w-full h-full bg-slate-100">
       <div className=" w-96 max-sm:w-3/4">
@@ -25,43 +46,61 @@ const AuthPage = () => {
           }
           content={
             <>
-              <CardContent>
-                <form>
-                  <div className="grid items-center w-full gap-4">
-                    <div className="flex flex-col space-y-1.5">
-                      <Label htmlFor="email">이메일</Label>
-                      <Input
-                        id="email"
-                        placeholder="email"
-                        onChange={(e) => {
-                          setEnteredEmail(e.target.value);
-                        }}
-                      />
-                    </div>
-                    <div className="flex flex-col space-y-1.5">
-                      <Label htmlFor="password">비밀번호</Label>
-                      <Input
-                        id="password"
-                        type="password"
-                        placeholder="password"
-                        onChange={(e) => {
-                          setEnteredPassword(e.target.value);
-                        }}
-                      />
-                    </div>
-                  </div>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <>
+                        <FormItem>
+                          <FormLabel htmlFor="email">이메일</FormLabel>
+                          <FormControl>
+                            <Input
+                              id="email"
+                              placeholder="example@domain.com"
+                              required={true}
+                              {...field}
+                            />
+                          </FormControl>
+
+                          {form.formState.errors.email && (
+                            <FormDescription>This is your public display name.</FormDescription>
+                          )}
+                        </FormItem>
+
+                        <FormMessage />
+                      </>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="pw"
+                    render={({ field }) => (
+                      <>
+                        <FormItem>
+                          <FormLabel htmlFor="pw">비밀번호</FormLabel>
+                          <Input
+                            id="pw"
+                            type="password"
+                            placeholder="password"
+                            required={true}
+                            {...field}
+                          />
+                          {form.formState.errors.pw && (
+                            <FormDescription>This is your public display name.</FormDescription>
+                          )}
+                        </FormItem>
+                        <FormMessage />
+                      </>
+                    )}
+                  />
+                  <Button type="submit" className="w-full">
+                    로그인
+                  </Button>
                 </form>
-              </CardContent>
-              <CardFooter className="flex justify-center">
-                <Button
-                  onClick={() => {
-                    console.log(enteredEmail, enteredPassword);
-                  }}
-                >
-                  로그인
-                </Button>
-              </CardFooter>
-              <CardFooter className="flex justify-around">
+              </Form>
+              <div className="flex justify-around mt-8">
                 <Link href={'/auth/forget'} className="cursor-pointer ">
                   비밀번호 찾기
                 </Link>
@@ -69,7 +108,7 @@ const AuthPage = () => {
                 <Link href={'/auth/signup'} className="cursor-pointer ">
                   회원가입
                 </Link>
-              </CardFooter>
+              </div>
             </>
           }
         ></Postit>
